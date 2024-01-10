f23edf44ec012b93600e56244c4b41e34aa40e40
flos-code <148456982+flos-code@users.noreply.github.com>
2024-01-10 07:24:47 +0100

    about section hover effects

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -8,5 +8,25 @@ import { Component } from '@angular/core';
   styleUrl: './about.component.scss',
 })
 export class AboutComponent {
-  imgSrcProfile: string = '/assets/img/Property 1=Default.png';
+  hoveredArrow: boolean = false;
+  hoveredArrowDelay: boolean = false;
+
+  arrowImagesLeft: string[] = [
+    '/assets/img/arrow_left_00.png',
+    '/assets/img/arrow_left_01.png',
+    '/assets/img/arrow_left_02.png',
+  ];
+
+  getImageSrc(): string {
+    if (this.hoveredArrow && !this.hoveredArrowDelay) {
+      setTimeout(() => {
+        this.hoveredArrowDelay = true;
+      }, 150);
+      return this.arrowImagesLeft[1];
+    } else if (this.hoveredArrow && this.hoveredArrowDelay) {
+      return this.arrowImagesLeft[2];
+    } else {
+      return this.arrowImagesLeft[0];
+    }
+  }
 }
