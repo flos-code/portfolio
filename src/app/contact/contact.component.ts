c0c89303c57b12d50eb59698204c65a93e5bbe9e
flos-code <148456982+flos-code@users.noreply.github.com>
2024-01-15 20:05:49 +0100

    added button to contact from

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { MatSelectModule } from '@angular/material/select';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -9,6 +11,7 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
   selector: 'app-contact',
   standalone: true,
   imports: [
+    FormsModule,
     MatFormFieldModule,
     MatInputModule,
     MatSelectModule,
@@ -18,4 +21,12 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss',
 })
-export class ContactComponent {}
+export class ContactComponent {
+  onSubmit(form: NgForm) {
+    if (form.valid) {
+      // Process your form
+    } else {
+      // Handle invalid form
+    }
+  }
+}
