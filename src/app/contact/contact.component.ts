d5e1deb53f7ebfbd4c80944b7c497a5d4ed4f07a
flos-code <148456982+flos-code@users.noreply.github.com>
2024-01-16 15:54:45 +0100

    form input validation

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { NgForm } from '@angular/forms';
 import { FormsModule } from '@angular/forms';
 import { MatSelectModule } from '@angular/material/select';
@@ -11,6 +12,7 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
   selector: 'app-contact',
   standalone: true,
   imports: [
+    CommonModule,
     FormsModule,
     MatFormFieldModule,
     MatInputModule,
@@ -22,6 +24,22 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
   styleUrl: './contact.component.scss',
 })
 export class ContactComponent {
+  inputName: string = '';
+  inputEmail: string = '';
+  inputMessage: string = '';
+  privacyPolicyChecked: boolean = false;
+
+  isNameValid(name: string): boolean {
+    // Überprüfe, ob der Name nur Buchstaben und Leerzeichen enthält
+    const regex = /^[a-zA-Z\sÖÄÜöäüß]*$/;
+    return regex.test(name);
+  }
+
+  isEmailValid(email: string): boolean {
+    const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    return regex.test(email);
+  }
+
   onSubmit(form: NgForm) {
     if (form.valid) {
       // Process your form
