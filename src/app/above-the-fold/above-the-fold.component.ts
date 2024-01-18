20d551665ccdf077fb992a0de9f0384ad9d8657e
flos-code <148456982+flos-code@users.noreply.github.com>
2024-01-18 22:31:06 +0100

    arrow animations added

--- src/app/above-the-fold/above-the-fold.component.ts
+++ src/app/above-the-fold/above-the-fold.component.ts
@@ -1,38 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-above-the-fold',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './above-the-fold.component.html',
   styleUrl: './above-the-fold.component.scss',
 })
-export class AboveTheFoldComponent {
-  arrowImages: string[] = [
-    '/assets/img/scroll-arrow-animation/Frame 5700.png',
-    '/assets/img/scroll-arrow-animation/Frame 5701.png',
-    '/assets/img/scroll-arrow-animation/Frame 5702.png',
-    '/assets/img/scroll-arrow-animation/Frame 5703.png',
-    '/assets/img/scroll-arrow-animation/Frame 5704.png',
-    '/assets/img/scroll-arrow-animation/Frame 5705.png',
-    '/assets/img/scroll-arrow-animation/Frame 5706.png',
-  ];
-  currentImageIndex: number = 0;
-  // private timerSubscription: Subscription = new Subscription();
-
-  // ngOnInit(): void {
-  //   const intervalTime = 1000; // 1 second interval
-  //   const maxIndex = this.arrowImages.length - 1;
-
-  //   this.timerSubscription = interval(intervalTime).subscribe(() => {
-  //     this.currentImageIndex = (this.currentImageIndex + 1) % (maxIndex + 1);
-  //     console.log(`Current Index: ${this.currentImageIndex}`);
-  //   });
-  // }
-
-  // ngOnDestroy() {
-  //   if (this.timerSubscription) {
-  //     this.timerSubscription.unsubscribe();
-  //   }
-  // }
-}
+export class AboveTheFoldComponent {}
