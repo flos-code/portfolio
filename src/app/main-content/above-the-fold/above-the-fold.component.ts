1617fb92fd6cc0c9f00a9c9c6128a8dfe204c465
flos-code <148456982+flos-code@users.noreply.github.com>
2024-01-26 14:41:16 +0100

    Above the fold translated

--- src/app/main-content/above-the-fold/above-the-fold.component.ts
+++ src/app/main-content/above-the-fold/above-the-fold.component.ts
@@ -1,11 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-above-the-fold',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TranslateModule],
   templateUrl: './above-the-fold.component.html',
   styleUrl: './above-the-fold.component.scss',
 })
-export class AboveTheFoldComponent {}
+export class AboveTheFoldComponent {
+  constructor(public translate: TranslateService) {}
+}
